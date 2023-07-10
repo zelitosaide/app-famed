ec382ff0876b6c1365b4ff237e7de3a9f0104f18
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 17:15:27 +0200

    Navbar

--- app/(website)/components/navbar.js
+++ app/(website)/components/navbar.js
@@ -12,22 +12,31 @@ export default function Navbar() {
         className="p-3 grow-0"
       />
       <ul className="uppercase flex text-[#178415] font-bold text-lg">
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Página Inicial
         </li>
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Ensino
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+          </svg>
         </li>
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Investigação
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+          </svg>
         </li>
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Extensão
         </li>
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Submissão de Protocolos
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+          </svg>
         </li>
-        <li className="p-3">
+        <li className="p-3 flex items-center">
           Notícias
         </li>
       </ul>
