6dc416530cffdeec501d98f2f5adbf6f53a6f626
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 16:49:49 +0200

    Navbar

--- app/(website)/components/navbar.js
+++ app/(website)/components/navbar.js
@@ -4,13 +4,21 @@ import logo from "@/public/logo.png";
 
 export default function Navbar() {
   return (
-    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }} className="pl-32 pr-32">
+    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }} className="pl-32 pr-32 flex items-center">
       <Image
         alt="Famed logo"
         src={logo}
         width={208}
-        className="p-3"
+        className="p-3 grow-0"
       />
+      <ul className="uppercase">
+        <li>Página Inicial</li>
+        <li>Ensino</li>
+        <li>Investigação</li>
+        <li>Extensão</li>
+        <li>Submissão de Protocolos</li>
+        <li>Notícias</li>
+      </ul>
     </div>
   );
 }
