fe70c5f7265a02f563c2869a56ccc6cc0c6e8dce
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 14:53:55 +0200

    Header

--- app/(website)/components/header.js
+++ app/(website)/components/header.js
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <header style={{ background: "rgb(var(--secondary-button-rgb))"}}>
-      Header
+      <ul className="text-gray-300 flex text-sm">
+        <li className="p-2">Sobre a Faculdade</li>
+        <li className="p-2">Links úteis</li>
+      </ul>
     </header>
   );
 }
