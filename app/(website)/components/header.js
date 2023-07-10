9cd5743f78e613ea0724bc5f2c8b89b27def6526
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 15:36:35 +0200

    Header

--- app/(website)/components/header.js
+++ app/(website)/components/header.js
@@ -2,13 +2,13 @@ export default function Header() {
   return (
     <header style={{ background: "rgb(var(--accent-rgb))" }}>
       <ul className="text-slate-200 flex text-sm">
-        <li className="pt-2 pb-2 pl-3 pr-3 hover:bg-stone-700 cursor-pointer flex">
+        <li className="pt-2 pb-2 pl-3 pr-3 hover:bg-[#0d5e0c] cursor-pointer flex">
           Sobre a Faculdade
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
           </svg>
         </li>
-        <li className="pt-2 pb-2 pl-3 pr-3 hover:bg-stone-700 cursor-pointer flex">
+        <li className="pt-2 pb-2 pl-3 pr-3 hover:bg-[#0d5e0c] cursor-pointer flex">
           Links úteis
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
