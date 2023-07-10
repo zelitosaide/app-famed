d9ae810bf71d1a91b03673e28655958d591b1622
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 16:37:52 +0200

    Navbar

--- app/(website)/components/navbar.js
+++ app/(website)/components/navbar.js
@@ -4,10 +4,12 @@ import logo from "@/public/logo.png";
 
 export default function Navbar() {
   return (
-    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }}>
+    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }} className="pl-32 pr-32">
       <Image
         alt="Famed logo"
         src={logo}
+        width={208}
+        className="p-3"
       />
     </div>
   );
