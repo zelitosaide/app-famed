44814627c478e7be55f98dba6c056bca97a56854
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-16 15:06:38 +0200

    Navigation menu

--- app/(website)/components/navbar.js
+++ app/(website)/components/navbar.js
@@ -34,8 +34,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Introduction</div>
-                            <p className="ListItemText">Build high-quality, accessible design systems and web apps.</p>
+                            <div className="ListItemHeading">Mestrado em Saúde Pública Presencial (MSP)</div>
+                            <p className="ListItemText">
+                              Formação multidisciplinar, orientada para a solução...
+                            </p>
                           </a>
                         </Link>
                       </li>
@@ -43,8 +45,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Getting started</div>
-                            <p className="ListItemText">A quick tutorial to get you up and running with Radix Primitives.</p>
+                            <div className="ListItemHeading">Mestrado em Saúde Pública à Distância (MSPD)</div>
+                            <p className="ListItemText">
+                              Mestrado em Saúde Pública à Distância (MSPD), com os seguintes ramos...
+                            </p>
                           </a>
                         </Link>
                       </li>
@@ -52,8 +56,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Styling</div>
-                            <p className="ListItemText">Unstyled and compatible with any styling solution.</p>
+                            <div className="ListItemHeading">Mestrado em Biociências (MBC)</div>
+                            <p className="ListItemText">
+                              Visa formar profissionais de nível superior qualificados...
+                            </p>
                           </a>
                         </Link>
                       </li>
@@ -61,8 +67,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Animation</div>
-                            <p className="ListItemText">Use CSS keyframes or any animation library of your choice.</p>
+                            <div className="ListItemHeading">Mestrado em Epidemiologia de Campo e Laboratorial (MECL)</div>
+                            <p className="ListItemText">
+                              Formação em serviço, baseada nos Princípios Fundamentais da...
+                            </p>
                           </a>
                         </Link>
                       </li>
@@ -70,8 +78,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Accessibility</div>
-                            <p className="ListItemText">Tested in a range of browsers and assistive technologies.</p>
+                            <div className="ListItemHeading">Mestrado em Saúde Mental e Psico-intervenções (MSMPI)</div>
+                            <p className="ListItemText">
+                              Formação de profissionais com competências em pesquisa, avaliação...
+                            </p>
                           </a>
                         </Link>
                       </li>
@@ -79,8 +89,10 @@ export default function Navbar() {
                       <li>
                         <Link asChild>
                           <a className="ListItemLink" href="/">
-                            <div className="ListItemHeading">Releases</div>
-                            <p className="ListItemText">Radix Primitives releases and their changelogs.</p>
+                            <div className="ListItemHeading">Mestrado em Emergências Pediatrias e Neonatais (MEPN)</div>
+                            <p className="ListItemText">
+                              Formação de profissionais de saúde para providenciar cuidados...
+                            </p>
                           </a>
                         </Link>
                       </li>
