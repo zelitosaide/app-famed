1e45ffda98a9d7152c160f79521fe7fc2a2c2b83
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-16 13:48:04 +0200

    Navigation menu

--- app/(website)/components/navbar.js
+++ app/(website)/components/navbar.js
@@ -5,12 +5,12 @@ import "./navbar.css";
 import Image from "next/image";
 import logo from "@/public/logo.png";
 
-import { Root, List, Item, Trigger, Link } from "@radix-ui/react-navigation-menu";
+import { Root, List, Item, Trigger, Link, Content } from "@radix-ui/react-navigation-menu";
 import { CaretDownIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
   return (
-    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }} className="pl-28 pr-28 flex items-center justify-between">
+    <div style={{ backgroundColor: "rgb(var(--card-rgb))" }} className="pl-28 pr-28 flex items-center justify-between relative">
       <Image
         alt="Famed logo"
         src={logo}
@@ -28,11 +28,75 @@ export default function Navbar() {
               <Trigger className="NavigationMenuTrigger">
                 Ensino <CaretDownIcon aria-hidden />
               </Trigger>
+              <div>
+                <Content>
+                  <div className="NavigationMenuContentBoundary">
+                    <ul className="List two">
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Introduction</div>
+                            <p className="ListItemText">Build high-quality, accessible design systems and web apps.</p>
+                          </a>
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Getting started</div>
+                            <p className="ListItemText">A quick tutorial to get you up and running with Radix Primitives.</p>
+                          </a>
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Styling</div>
+                            <p className="ListItemText">Unstyled and compatible with any styling solution.</p>
+                          </a>
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Animation</div>
+                            <p className="ListItemText">Use CSS keyframes or any animation library of your choice.</p>
+                          </a>
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Accessibility</div>
+                            <p className="ListItemText">Tested in a range of browsers and assistive technologies.</p>
+                          </a>
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link asChild>
+                          <a className="ListItemLink" href="/">
+                            <div className="ListItemHeading">Releases</div>
+                            <p className="ListItemText">Radix Primitives releases and their changelogs.</p>
+                          </a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </Content>
+              </div>
             </Item>
             <Item>
               <Trigger className="NavigationMenuTrigger">
                 Investigação <CaretDownIcon aria-hidden />
               </Trigger>
+              <div>
+
+              </div>
             </Item>
             <Item>
               <Link className="NavigationMenuLink" href="/">Extensão</Link>
@@ -41,6 +105,9 @@ export default function Navbar() {
               <Trigger className="NavigationMenuTrigger">
                 Submissão de Protocolos <CaretDownIcon aria-hidden />
               </Trigger>
+              <div>
+
+              </div>
             </Item>
             <Item>
               <Link className="NavigationMenuLink" href="/">Notícias</Link>
@@ -48,38 +115,6 @@ export default function Navbar() {
           </List>
         </Root>
       </div>
-
-
-
-      {/* <ul className="uppercase flex text-[#178415] font-bold text-lg">
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Página Inicial
-        </li>
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Ensino
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-          </svg>
-        </li>
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Investigação
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-          </svg>
-        </li>
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Extensão
-        </li>
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Submissão de Protocolos
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-          </svg>
-        </li>
-        <li className="p-3 flex items-center cursor-pointer hover:underline underline-offset-8 decoration-2 decoration-[#6BAA65]">
-          Notícias
-        </li>
-      </ul> */}
     </div>
   );
 }
