c557eef16ad41f00882d127dcb37923518bfcb44
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-17 15:44:18 +0200

    Header

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,11 +1,8 @@
 import "../globals.css";
-// import { Inter } from "next/font/google";
 
-import Header from "./components/header";
+import Header from "./components/header/header";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar/navbar";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "FAMED",
@@ -15,7 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}> */}
       <body>
         <Header />
         <Navbar />
