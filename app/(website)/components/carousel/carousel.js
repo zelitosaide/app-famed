def03d434df7534d713f8afe5e0d970debb7ed0b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-19 15:43:09 +0200

    Carousel

--- app/(website)/components/carousel/carousel.js
+++ app/(website)/components/carousel/carousel.js
@@ -1,5 +1,27 @@
+"use client";
+
 import "./carousel.css";
 
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import image1 from "@/public/image-1.jpeg";
+
 export default function Carousel() {
-  return;
+  return (
+    <Swiper>
+      <SwiperSlide>
+        <Image
+          src={image1}
+          quality={100}
+        />
+        <div>
+          <h2></h2>
+          <p></p>
+          {/* Link */}
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
 }
