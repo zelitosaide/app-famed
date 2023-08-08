7dedc1b92438b512132d85964045dfcc2dc63422
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-08 15:31:36 +0200

    Header links

--- app/(website)/components/header/header.js
+++ app/(website)/components/header/header.js
@@ -9,65 +9,6 @@ import { CaretDownIcon, InstagramLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } f
 import { Item, List, Root, Trigger, Content } from "@radix-ui/react-navigation-menu";
 import { Separator } from "@radix-ui/react-separator";
 
-export const links = [
-  { 
-    title: "Sobre a Faculdade",
-    segment: "sobre-a-faculdade",
-    children: [
-      { 
-        title: "Sobre a Faculdade de Medicina",
-        segment: "sobre-a-faculdade-de-medicina",
-      },
-      { 
-        title: "Documentos e regumentos da Faculdade",
-        segment: "documentos-e-regumentos-da-faculdade",
-      },
-      { 
-        title: "Departamentos e Unidades",
-        segment: "departamentos-e-unidades",
-      },
-    ]
-  },
-  { 
-    title: "Links úteis", 
-    segment: "links-uteis",
-    children: [
-      { 
-        title: "Plataforma de Ensino Online (VULA)", 
-        href: "",
-      },
-      { 
-        title: "Base de Dados (RedCap)",
-        href: "",
-      },
-      { 
-        title: "Sistema Integrado de Gestão Académica (SIGA)",
-        href: "",
-      },
-      { 
-        title: "Universidade Eduardo Mondlane (UEM)",
-        href: "",
-      },
-      { 
-        title: "Direção do Registo Académico (UEM)",
-        href: "",
-      },
-      { 
-        title: "Departamento de Admissão a Univerdade",
-        href: "",
-      },
-    ]
-  },
-  {
-    title: "Consultas de Bioestatística",
-    segment: "consultas-de-bioestatistica"
-  },
-  {
-    title: "Cursos de curta duração",
-    segment: "cursos-de-curta-duracao"
-  }
-];
-
 export default async function Header() {
   return (
     <header style={{ backgroundColor: "rgb(var(--accent-rgb))" }} className="pl-28 pr-28 flex justify-between">
