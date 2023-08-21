a595ae22613271bc6e410c1651e9d014aec72150
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-21 23:20:06 +0200

    Page Layout

---
+++ app/(website)/ensino/graduacao/[slug]/layout.js
@@ -0,0 +1,18 @@
+export default function Layout({ children }) {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "26%", padding: "30px 20px 20px 120px" }}>
+        <div 
+          style={{ 
+            width: "100%", 
+            background: "rgb(23 132 21 / 12%)", 
+            height: 400 
+          }} 
+        />
+      </div>
+      <div style={{ padding: "30px 20px"}}>
+        {children}
+      </div>
+    </div>
+  );
+}
