a1c164002797f38c73415f05e4761ac0dc6d58f8
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-03 22:25:30 +0200

    TipTap

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,8 +1,8 @@
 import "../globals.css";
 
-import HeaderContainer from "./components/header/header-container";
+// import HeaderContainer from "./components/header/header-container";
 import Footer from "./components/footer";
-import NavbarContainer from "./components/navbar/navbar-container";
+// import NavbarContainer from "./components/navbar/navbar-container";
 
 export const metadata = {
   title: "FAMED",
--- app/api/server.js
+++ app/api/server.js
@@ -1,4 +1,6 @@
 export async function getLinks(category) {
-  const res = await fetch(`http://localhost:3001/links/category/${category}`);
+  const res = await fetch(`http://localhost:3001/links/category/${category}`, {
+    cache: "no-cache"
+  });
   return res.json();
 }
