a4100ba0f958be19cdcaff014a39dafdfb56a61d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-06 10:11:28 +0200

    Navigation

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <HeaderContainer />
-        {/* <NavbarContainer /> */}
+        <NavbarContainer />
         {children}
         <Footer />
       </body>
