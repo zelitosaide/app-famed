fa36622f080e22842aaec7bcf7358d7d89065104
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-08 20:21:58 +0200

    TipTap

--- app/(website)/tip-tap.js
+++ app/(website)/tip-tap.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
+import { BubbleMenu, EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Document from "@tiptap/extension-document";
 import Placeholder from "@tiptap/extension-placeholder";
@@ -8,6 +8,14 @@ import Heading from "@tiptap/extension-heading";
 import Youtube from "@tiptap/extension-youtube";
 import Dropcursor from "@tiptap/extension-dropcursor";
 import Image from "@tiptap/extension-image";
+// import Color from "@tiptap/extension-color";
+// import TextStyle from "@tiptap/extension-text-style";
+// import ListItem from "@tiptap/extension-list-item";
+import Highlight from "@tiptap/extension-highlight";
+import TextAlign from "@tiptap/extension-text-align";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Link from "@tiptap/extension-link";
 
 import { useCallback } from "react";
 
@@ -18,12 +26,36 @@ const CustomDocument = Document.extend({
 export default function TipTap({ content }) {
   const editor = useEditor({
     extensions: [
+      Link.configure({
+        openOnClick: true,
+        autolink: true,
+        linkOnPaste: true,
+        validate: href => /^https?:\/\//.test(href),
+      }),
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+      Highlight,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      // Color.configure({ types: [TextStyle.name, ListItem.name] }),
+      // TextStyle.configure({ types: [ListItem.name] }),
       CustomDocument,
       Heading.configure({
-        levels: [1, 2, 3],
+        levels: [1, 2],
       }),
       StarterKit.configure({
         document: false,
+        // bulletList: {
+        //   keepMarks: true,
+        //   keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        // },
+        // orderedList: {
+        //   keepMarks: true,
+        //   keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        // },
         // history: false,
       }),
       Youtube.configure({
@@ -34,10 +66,12 @@ export default function TipTap({ content }) {
       Dropcursor,
       Placeholder.configure({
         placeholder: ({ node }) => {
-          if (node.type.name === 'heading') {
+          console.log(node.attrs.level)
+
+          if (node.type.name === 'heading' && node.attrs?.level === 1) {
             return "What’s the title?";
           }
-          return "Can you add some further context?";
+          return "Type the content...";
         },
       }),
     ],
@@ -62,17 +96,178 @@ export default function TipTap({ content }) {
     }
   }, [editor]);
 
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink()
+        .run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+      .run()
+  }, [editor])
+
   if (!editor) {
     return null;
   }
 
   return (
     <div className="prose prose-zinc prose-h1:text-2xl prose-h1:uppercase prose-h1:font-bold prose-h1:text-[#178415] prose-h2:text-xl prose-h2:text-[#178415] prose-h2:font-bold marker:text-[#178415] max-w-none">
-      {!!editor && (
-        <button onClick={addImage}>setImage</button>
-      )}
-      <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
-      </FloatingMenu>
+      <button onClick={addImage}>setImage</button>
+      {editor && <BubbleMenu className="bubble-menu" tippyOptions={{ duration: 100 }} editor={editor}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          Bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          Strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+          className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+        >
+          purple
+        </button>
+        <button onClick={() => editor.chain().focus().toggleHighlight().run()} className={editor.isActive('highlight') ? 'is-active' : ''}>
+          highlight
+        </button>
+
+        <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
+          left
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
+          center
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
+          right
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('justify').run()} className={editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''}>
+          justify
+        </button>
+
+        <button onClick={setLink} className={editor.isActive('link') ? 'is-active' : ''}>
+          setLink
+        </button>
+        <button
+          onClick={() => editor.chain().focus().unsetLink().run()}
+          disabled={!editor.isActive('link')}
+        >
+          unsetLink
+        </button>
+      </BubbleMenu>}
+
+      {editor && <FloatingMenu className="floating-menu" tippyOptions={{ duration: 100 }} editor={editor}>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          H1
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          H2
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          Bullet List
+        </button>
+
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          Bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          Strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive('paragraph') ? 'is-active' : ''}
+        >
+          paragraph
+        </button>
+
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          ordered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? 'is-active' : ''}
+        >
+          blockquote
+        </button>
+        <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          horizontal rule
+        </button>
+        <button onClick={() => editor.chain().focus().toggleHighlight().run()} className={editor.isActive('highlight') ? 'is-active' : ''}>
+          highlight
+        </button>
+
+        <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
+          left
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
+          center
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
+          right
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('justify').run()} className={editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''}>
+          justify
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleTaskList().run()}
+          className={editor.isActive('taskList') ? 'is-active' : ''}
+        >
+          toggleTaskList
+        </button>
+      </FloatingMenu>}
+
       <EditorContent  editor={editor} />
     </div>
   );
