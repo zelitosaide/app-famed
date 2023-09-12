6aaafd26a27387fc9e9f77d1c60371094fc68cd0
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-12 09:47:13 +0200

    TipTap

--- app/(website)/components/tip-tap/content.js
+++ app/(website)/components/tip-tap/content.js
@@ -153,10 +153,10 @@ export default function Content({ content, id }) {
       
     }), Image, Dropcursor],
     content: `
-        <p>This is a basic example of implementing images. Drag to re-order.</p>
-        <img src="https://source.unsplash.com/8xznAGy4HcY/800x400" />
-        <img src="https://source.unsplash.com/K9QHL52rE2k/800x400" />
-      `,
+      <p>This is a basic example of implementing images. Drag to re-order.</p>
+      <img src="https://source.unsplash.com/8xznAGy4HcY/800x400" />
+      <img src="https://source.unsplash.com/K9QHL52rE2k/800x400" />
+    `,
   })
 
   const addImage = useCallback(() => {
