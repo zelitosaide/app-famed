474b6f908bf2bc9161dceae2f12b51faf617bba2
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-13 13:11:57 +0200

    TipTap

--- app/(website)/components/tip-tap/menu.js
+++ app/(website)/components/tip-tap/menu.js
@@ -51,45 +51,45 @@ export default function Menu({ editor }) {
   }
 
   const items = [
-    {
-      icon: 'h-1',
-      title: 'Heading 1',
-      action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      isActive: () => editor.isActive('heading', { level: 1 }),
-    },
-    {
-      icon: 'h-2',
-      title: 'Heading 2',
-      action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      isActive: () => editor.isActive('heading', { level: 2 }),
-    },
-    {
-      icon: 'paragraph',
-      title: 'Paragraph',
-      action: () => editor.chain().focus().setParagraph().run(),
-      isActive: () => editor.isActive('paragraph'),
-    },
-    {
-      icon: 'list-unordered',
-      title: 'Bullet List',
-      action: () => editor.chain().focus().toggleBulletList().run(),
-      isActive: () => editor.isActive('bulletList'),
-    },
-    {
-      icon: 'list-ordered',
-      title: 'Ordered List',
-      action: () => editor.chain().focus().toggleOrderedList().run(),
-      isActive: () => editor.isActive('orderedList'),
-    },
-    {
-      icon: 'list-check-2',
-      title: 'Task List',
-      action: () => editor.chain().focus().toggleTaskList().run(),
-      isActive: () => editor.isActive('taskList'),
-    },
-    {
-      type: 'divider',
-    },
+    // {
+    //   icon: 'h-1',
+    //   title: 'Heading 1',
+    //   action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+    //   isActive: () => editor.isActive('heading', { level: 1 }),
+    // },
+    // {
+    //   icon: 'h-2',
+    //   title: 'Heading 2',
+    //   action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+    //   isActive: () => editor.isActive('heading', { level: 2 }),
+    // },
+    // {
+    //   icon: 'paragraph',
+    //   title: 'Paragraph',
+    //   action: () => editor.chain().focus().setParagraph().run(),
+    //   isActive: () => editor.isActive('paragraph'),
+    // },
+    // {
+    //   icon: 'list-unordered',
+    //   title: 'Bullet List',
+    //   action: () => editor.chain().focus().toggleBulletList().run(),
+    //   isActive: () => editor.isActive('bulletList'),
+    // },
+    // {
+    //   icon: 'list-ordered',
+    //   title: 'Ordered List',
+    //   action: () => editor.chain().focus().toggleOrderedList().run(),
+    //   isActive: () => editor.isActive('orderedList'),
+    // },
+    // {
+    //   icon: 'list-check-2',
+    //   title: 'Task List',
+    //   action: () => editor.chain().focus().toggleTaskList().run(),
+    //   isActive: () => editor.isActive('taskList'),
+    // },
+    // {
+    //   type: 'divider',
+    // },
     {
       icon: 'bold',
       title: 'Bold',
@@ -141,33 +141,33 @@ export default function Menu({ editor }) {
       action: () => editor.chain().focus().setTextAlign('justify').run(),
       isActive: () => editor.isActive({ textAlign: 'justify' }),
     },
+    // {
+    //   type: 'divider',
+    // },
+    // {
+    //   icon: 'double-quotes-l',
+    //   title: 'Blockquote',
+    //   action: () => editor.chain().focus().toggleBlockquote().run(),
+    //   isActive: () => editor.isActive('blockquote'),
+    // },
+    // {
+    //   icon: 'separator',
+    //   title: 'Horizontal Rule',
+    //   action: () => editor.chain().focus().setHorizontalRule().run(),
+    // },
     {
       type: 'divider',
     },
-    {
-      icon: 'double-quotes-l',
-      title: 'Blockquote',
-      action: () => editor.chain().focus().toggleBlockquote().run(),
-      isActive: () => editor.isActive('blockquote'),
-    },
-    {
-      icon: 'separator',
-      title: 'Horizontal Rule',
-      action: () => editor.chain().focus().setHorizontalRule().run(),
-    },
-    {
-      type: 'divider',
-    },
-    {
-      icon: 'image-line',
-      title: 'Image',
-      action: addImage,
-    },
-    {
-      icon: 'youtube-line',
-      title: 'YouTube Video',
-      action: addYoutubeVideo,
-    },
+    // {
+    //   icon: 'image-line',
+    //   title: 'Image',
+    //   action: addImage,
+    // },
+    // {
+    //   icon: 'youtube-line',
+    //   title: 'YouTube Video',
+    //   action: addYoutubeVideo,
+    // },
     {
       icon: 'link',
       title: 'Link',
@@ -180,24 +180,24 @@ export default function Menu({ editor }) {
       action: () => editor.chain().focus().unsetLink().run(),
       disabled: !editor.isActive('link'),
     },
-    {
-      type: 'divider',
-    },
-    {
-      icon: 'format-clear',
-      title: 'Clear Format',
-      action: () => editor.chain().focus().clearNodes().unsetAllMarks().run(),
-    },
-    {
-      icon: 'arrow-go-back-line',
-      title: 'Undo',
-      action: () => editor.chain().focus().undo().run(),
-    },
-    {
-      icon: 'arrow-go-forward-line',
-      title: 'Redo',
-      action: () => editor.chain().focus().redo().run(),
-    },
+    // {
+    //   type: 'divider',
+    // },
+    // {
+    //   icon: 'format-clear',
+    //   title: 'Clear Format',
+    //   action: () => editor.chain().focus().clearNodes().unsetAllMarks().run(),
+    // },
+    // {
+    //   icon: 'arrow-go-back-line',
+    //   title: 'Undo',
+    //   action: () => editor.chain().focus().undo().run(),
+    // },
+    // {
+    //   icon: 'arrow-go-forward-line',
+    //   title: 'Redo',
+    //   action: () => editor.chain().focus().redo().run(),
+    // },
   ];
 
 
