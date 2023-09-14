0d9737609978f9741ea1002dcdf16170d49f73ba
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-15 01:02:27 +0200

    Side Nav

--- app/(website)/components/sidenav/accordion.js
+++ app/(website)/components/sidenav/accordion.js
@@ -1,5 +1,33 @@
 "use client";
 
+import { Root, Item, Header, Trigger } from "@radix-ui/react-accordion";
+import { forwardRef } from "react";
+
 export default function Accordion({ links }) {
-  return;
+  return (
+    <Root className="AccordionRoot" type="multiple">
+      {links.map(function(link) {
+        return (
+          <Item className="AccordionItem" key={link._id} value={link._id}>
+            <AccordionTrigger>
+              {link.title}
+            </AccordionTrigger>
+          </Item>
+        );
+      })}
+    </Root>
+  );
 }
+
+const AccordionTrigger = forwardRef(({ children, className, ...props }, forwardedRef) => (
+  <Header className="AccordionHeader">
+    <Trigger
+      className={"AccordionTrigger"}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+      {/* <ChevronDownIcon className="AccordionChevron" aria-hidden /> */}
+    </Trigger>
+  </Header>
+));
