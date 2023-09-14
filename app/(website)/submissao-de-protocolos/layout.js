2e762a8177f4b7a812baa468fdbfa57777ec68a2
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-14 23:24:58 +0200

    Side Nav

--- app/(website)/submissao-de-protocolos/layout.js
+++ app/(website)/submissao-de-protocolos/layout.js
@@ -5,9 +5,11 @@ export default function Layout({ children }) {
         <div 
           style={{ 
             width: "100%", 
-            background: "rgb(23 132 21 / 12%)", 
+            // background: "rgb(23 132 21 / 12%)", 
+            // background: "#1b9a19", 
             height: 400 
           }} 
+          className="bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-300 via-emerald-600 to-lime-500"
         />
       </div>
       <div style={{ padding: "30px 120px 30px 0", width: "74%" }}>
@@ -16,3 +18,4 @@ export default function Layout({ children }) {
     </div>
   );
 }
+
