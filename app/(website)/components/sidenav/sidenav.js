92ca14bdae4a9409c7ab2c8628026cb72948f31d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-15 00:19:05 +0200

    Side Nav

--- app/(website)/components/sidenav/sidenav.js
+++ app/(website)/components/sidenav/sidenav.js
@@ -2,8 +2,15 @@ import { getLinkByTitle } from "@/app/api/server";
 
 export default async function SideNav() {
   const sobreNos = getLinkByTitle("Sobre a Faculdade");
+  const ensino = getLinkByTitle("Ensino");
+  const investigacao = getLinkByTitle("Investigação");
+  const extensao = getLinkByTitle("Extensão");
+  const subProtocolos = getLinkByTitle("Submissão de Protocolos");
+  const noticias = getLinkByTitle("Notícias");
 
-  // console.log(sobreNos);
+  const links = await Promise.all([
+    sobreNos, ensino, investigacao, extensao, subProtocolos, noticias
+  ]);
 
   return (
     <div 
