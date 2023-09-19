72ace614cdc11219af62185dd585b42117411f7c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-19 18:01:46 +0200

    Footer

--- app/(website)/components/footer.js
+++ app/(website)/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div style={{ height: 260 }} className="bg-[#074F07] pl-28 pr-28">
+    <div style={{ height: 260 }} className="bg-[#074F07] pl-28 pr-28 pt-10">
       <div className="grid gap-4 grid-cols-3">
         <div className="bg-[red]">
           <h2>Faculdade de Medicina, UEM</h2>
