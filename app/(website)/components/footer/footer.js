919e596e4d57bfb354966ca1d1851bc7253d1d1c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-19 18:14:30 +0200

    Footer

--- app/(website)/components/footer/footer.js
+++ app/(website)/components/footer/footer.js
@@ -1,15 +1,17 @@
+import "./footer.css";
+
 export default function Footer() {
   return (
-    <div style={{ height: 260 }} className="bg-[#074F07] pl-28 pr-28 pt-10">
+    <div style={{ height: 260 }} className="bg-[#074F07] pl-28 pr-28 pt-9">
       <div className="grid gap-4 grid-cols-3">
-        <div className="bg-[red]">
-          <h2>Faculdade de Medicina, UEM</h2>
+        <div>
+          <h2 className="footer-title">Faculdade de Medicina, UEM</h2>
         </div>
-        <div className="bg-[yellow]">
-          <h2>Links úteis</h2>
+        <div>
+          <h2 className="footer-title">Links úteis</h2>
         </div>
-        <div className="bg-[white]">
-          <h2>Contacto</h2>
+        <div>
+          <h2 className="footer-title">Contacto</h2>
         </div>
       </div>
     </div>
