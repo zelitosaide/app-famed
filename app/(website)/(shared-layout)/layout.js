235b3c90810b34b6efc7eb18bf5785484d7a623b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-24 13:09:33 +0200

    layout

--- app/(website)/(shared-layout)/layout.js
+++ app/(website)/(shared-layout)/layout.js
@@ -2,11 +2,11 @@ import SideNav from "../components/sidenav/sidenav";
 
 export default function Layout({ children }) {
   return (
-    <div style={{ display: "flex" }}>
-      <div className="pl-28" style={{ width: "32%", padding: "30px 20px 20px 120px" }}>
+    <div className="pl-28 pr-28" style={{ display: "flex" }}>
+      <div>
         <SideNav />
       </div>
-      <div className="pr-28" style={{ paddingTop: 30, paddingBottom: 30, width: "68%" }}>
+      <div>
         {children}
       </div>
     </div>
