1428d6c6389ddcb7e0f316a109f8f1491f945ce4
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-24 15:14:22 +0200

    Mestrado

--- app/(website)/(shared-layout)/ensino/mestrado/page.js
+++ app/(website)/(shared-layout)/ensino/mestrado/page.js
@@ -1,7 +1,27 @@
-export default function Page() {
+import { getLinkByTitle } from "@/app/api/server";
+import Link from "next/link";
+
+export default async function Page() {
+  const { children } = await getLinkByTitle("Ensino");
+  const { children: mestrados} = children.find(function(child) {
+    return child.segment === "mestrado";
+  });
+
   return (
     <div className="pt-2.5 pr-3 pl-5 pb-5">
       <h1>Mestrado</h1>
+      <ul>
+        {mestrados.map(function(link) {
+          return (
+            <li key={link.segment}>
+              <Link 
+                className="block" href={`/ensino/mestrado/${link.segment}`}>
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
