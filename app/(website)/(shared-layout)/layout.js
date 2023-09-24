5f6a629f422a814a3a6464a1f5cda85a12aff615
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-24 13:16:22 +0200

    layout

--- app/(website)/(shared-layout)/layout.js
+++ app/(website)/(shared-layout)/layout.js
@@ -3,7 +3,7 @@ import SideNav from "../components/sidenav/sidenav";
 export default function Layout({ children }) {
   return (
     <div className="pl-28 pr-28 flex">
-      <div>
+      <div className="shrink-0 w-72">
         <SideNav />
       </div>
       <div>
