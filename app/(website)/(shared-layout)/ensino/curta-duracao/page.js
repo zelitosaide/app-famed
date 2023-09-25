361eeaf32218090a4e0cacf7f271beea6a0380dc
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-25 11:49:37 +0200

    Cursos de curta duracao

--- app/(website)/(shared-layout)/ensino/curta-duracao/page.js
+++ app/(website)/(shared-layout)/ensino/curta-duracao/page.js
@@ -7,16 +7,17 @@ export default async function Page() {
   return (
     <div className="pt-2.5 pr-3 pl-5 pb-5">
       <h1>Cursos de curta duração</h1>
-      <ul className="flex gap-5">
+      <ul className="grid grid-cols-3 gap-x-5 gap-y-7">
         {cursosCurtaDuracao.map(function(curso) {
           return (
-            <li key={curso._id} className="bg-[red]">
+            <li key={curso._id} className="p-3 bg-[#ffffff] divide-y-4 divide-[#78ba78] rounded shadow-md shadow-[#ddeedd] border border-[#ddeedd]">
               <Link
-                className="block" 
+                className="text-base font-bold pb-3 block text-[#178415] hover:underline"
                 href={`/ensino/curta-duracao/${curso._id}`}
               >
                 {curso.title}
               </Link>
+              <p style={{ fontSize: 15 }} className="pt-3 pb-2 text-zinc-500">{curso.description}</p>
             </li>
           );
         })}
