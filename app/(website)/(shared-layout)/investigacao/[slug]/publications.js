d890a7cbaae1e039d0a2ac8899315ca4ab6a9f01
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-25 20:10:31 +0200

    Publications

--- app/(website)/(shared-layout)/investigacao/[slug]/publications.js
+++ app/(website)/(shared-layout)/investigacao/[slug]/publications.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Publications({ publications }) {
   return (
-    <ul>
+    <ul className="grid grid-cols-1 gap-y-7">
       {publications.map(function(pub) {
         return (
-          <li key={pub._id}>{pub.title}</li>
+          <li 
+            key={pub._id}
+            className="p-3 bg-[#ffffff] divide-y divide-[#78ba78] rounded shadow-sm shadow-[#ddeedd] border border-[#ddeedd]"
+          >
+            <Link 
+              href={pub.url} 
+              target="_blank"
+              className="text-base font-bold pb-3 block text-[#178415] hover:underline"
+            >
+              {pub.title}
+            </Link>
+            <p>
+              {pub.authors.join(", ")}
+            </p>
+          </li>
         );
       })}
     </ul>
