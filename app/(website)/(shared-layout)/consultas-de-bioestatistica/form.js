a6410d345b6131285eb078762cf47e8a678bf0bd
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 13:53:44 +0200

    Consultas

--- app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
+++ app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
@@ -1,15 +1,27 @@
 "use client";
 
-import { Flex, TextField, Theme } from "@radix-ui/themes";
+import { Box, Flex, Grid, TextField, Theme } from "@radix-ui/themes";
 import '@radix-ui/themes/styles.css';
 
 export default function Form() {
   return (
     <form>
       <Theme>
-        <Flex direction="column" gap="3" style={{ maxWidth: 400 }}>
-          <TextField.Input radius="none" placeholder="Search the docs…" />
-          <TextField.Input radius="large" placeholder="Search the docs…" />
+        <Flex direction="column" gap="3">
+          <Grid columns="2" gap="3" width="auto">
+            <Box>
+              <TextField.Input radius="large" placeholder="Search the docs…" />
+            </Box>
+            <Box>
+              <TextField.Input radius="large" placeholder="Search the docs…" />
+            </Box>
+            <Box>
+              <TextField.Input radius="large" placeholder="Search the docs…" />
+            </Box>
+            <Box>
+              <TextField.Input radius="large" placeholder="Search the docs…" />
+            </Box>
+          </Grid>
         </Flex>
       </Theme>
     </form>
