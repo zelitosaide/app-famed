4a573042fcda30cdd6eec00059fc25d6424b5a0c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 16:03:50 +0200

    Consultas

--- app/(website)/(shared-layout)/consultas-de-bioestatistica/page.js
+++ app/(website)/(shared-layout)/consultas-de-bioestatistica/page.js
@@ -5,7 +5,10 @@ export default async function Page() {
     <div className="pt-2.5 pr-3 pl-5 pb-5">
       <h1>Formulário de requisição de Consulta de Bioestatística</h1>
       <div className="rounded shadow-sm shadow-[#ddeedd] border border-[#ddeedd] p-8">
-        Caro(a) solicitante, Preencha o Formulário de requisição de Consulta de Bioestatística
+        
+        <p className="text-zinc-500 mb-5">
+          Caro(a) solicitante, Preencha o Formulário de requisição de Consulta de Bioestatística
+        </p>
 
         <Form />
 
