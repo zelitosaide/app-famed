1aa091df2f7c04610a2d2ba6ca0d6fb06bc09f18
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 09:15:24 +0200

    Noticias

---
+++ app/(website)/(shared-layout)/noticias/news.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+export default function News({ news }) {
+  return (
+    <ul className="grid grid-cols-1 gap-y-7">
+      {news.map(function(noticia) {
+        return (
+          <li 
+            key={noticia._id}
+            className="overflow-hidden bg-[#ffffff] rounded shadow-sm shadow-[#ddeedd] border border-[#ddeedd]"
+          >
+            <div className="flex">
+              {/* <img 
+                className="shrink-0 w-40"
+                src={`http://localhost:3001/${noticia.thumbnail}`}
+              /> */}
+              <div className="p-3">
+                <Link
+                  href={`/noticias/${noticia._id}`}
+                  className="text-base font-bold pb-3 text-[#178415] hover:underline"
+                >
+                  {noticia.title}
+                </Link>
+                <p style={{ fontSize: 15 }} className="text-[#C7681C] pt-1">
+                  Data da Publicação: 
+                </p>
+                <p style={{ fontSize: 15 }} className="text-zinc-500 pt-2">
+                  {/* {noticia.description} */}
+                  {noticia.content.slice(0, 200)}
+                </p>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- app/(website)/(shared-layout)/noticias/page.js
+++ app/(website)/(shared-layout)/noticias/page.js
@@ -1,7 +1,11 @@
+import News from "./news";
+
 export default function Page() {
   return (
     <div className="pt-2.5 pr-3 pl-5 pb-5">
       <h1>Notícias</h1>
+
+      <News news={data} />
     </div>
   );
 }
