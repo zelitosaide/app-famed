0dec9ed6bb78f7df7b5592b74f5d60023696af07
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 14:20:59 +0200

    Consultas

--- app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
+++ app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
@@ -10,41 +10,45 @@ export default function Form() {
         <Flex direction="column" gap="3">
           <Grid columns="2" gap="4" width="auto">
             <Box>
-              <Text size="2" weight="bold">
+              <Text size="2" weight="bold" as="p" mb="2">
                 Nome Completo
               </Text>
               <TextField.Input 
-                radius="large" 
+                radius="medium" 
+                size="3"
                 placeholder="Search the docs…"
                 color="green"
               />
             </Box>
             <Box>
-              <Text size="2" weight="bold">
+              <Text size="2" weight="bold" as="p" mb="2">
                 E-mail
               </Text>
               <TextField.Input 
-                radius="large" 
+                radius="medium" 
+                size="3"
                 placeholder="Search the docs…"
                 color="green"
               />
             </Box>
             <Box>
-              <Text size="2" weight="bold">
+              <Text size="2" weight="bold" as="p" mb="2">
                 Contacto telemóvel
               </Text>
               <TextField.Input 
-                radius="large" 
+                radius="medium" 
+                size="3"
                 placeholder="Search the docs…"
                 color="green"
               />
             </Box>
             <Box>
-              <Text size="2" weight="bold">
+              <Text size="2" weight="bold" as="p" mb="2">
                 Tópico de consulta
               </Text>
               <TextField.Input 
-                radius="large" 
+                radius="medium" 
+                size="3"
                 placeholder="Search the docs…"
                 color="green"
               />
