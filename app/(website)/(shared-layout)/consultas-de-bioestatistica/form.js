3359a76da709886a31bfdf66d1860d985b8a15d1
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 14:10:38 +0200

    Consultas

--- app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
+++ app/(website)/(shared-layout)/consultas-de-bioestatistica/form.js
@@ -8,30 +8,46 @@ export default function Form() {
     <form>
       <Theme>
         <Flex direction="column" gap="3">
-          <Grid columns="2" gap="3" width="auto">
+          <Grid columns="2" gap="4" width="auto">
             <Box>
               <Text size="2" weight="bold">
                 Nome Completo
               </Text>
-              <TextField.Input radius="large" placeholder="Search the docs…" />
+              <TextField.Input 
+                radius="large" 
+                placeholder="Search the docs…"
+                color="green"
+              />
             </Box>
             <Box>
               <Text size="2" weight="bold">
                 E-mail
               </Text>
-              <TextField.Input radius="large" placeholder="Search the docs…" />
+              <TextField.Input 
+                radius="large" 
+                placeholder="Search the docs…"
+                color="green"
+              />
             </Box>
             <Box>
               <Text size="2" weight="bold">
                 Contacto telemóvel
               </Text>
-              <TextField.Input radius="large" placeholder="Search the docs…" />
+              <TextField.Input 
+                radius="large" 
+                placeholder="Search the docs…"
+                color="green"
+              />
             </Box>
             <Box>
               <Text size="2" weight="bold">
                 Tópico de consulta
               </Text>
-              <TextField.Input radius="large" placeholder="Search the docs…" />
+              <TextField.Input 
+                radius="large" 
+                placeholder="Search the docs…"
+                color="green"
+              />
             </Box>
           </Grid>
         </Flex>
