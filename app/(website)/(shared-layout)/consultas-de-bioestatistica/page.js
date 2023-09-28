d8fc97e32ced4b9f94447b6dc3f9061326ee84be
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-28 15:26:48 +0200

    Consultas

--- app/(website)/(shared-layout)/consultas-de-bioestatistica/page.js
+++ app/(website)/(shared-layout)/consultas-de-bioestatistica/page.js
@@ -9,28 +9,6 @@ export default async function Page() {
 
         <Form />
 
-        {/* <form>
-          Nome Completo
-          <input type="text" className="block" style={{ border: "1px solid black" }} />
-
-          E-mail
-          <input type="email" className="block" style={{ border: "1px solid black" }} />
-
-          Contacto telemóvel
-          <input type="text" className="block" style={{ border: "1px solid black" }} />
-
-          Marque a sua consulta (apenas em todas Quarta-feira, das 16-18h)
-          <input type="text" className="block" style={{ border: "1px solid black" }} />
-
-          Tópico de consulta
-          <input type="text" className="block" style={{ border: "1px solid black" }} />
-
-          Resumo da pesquisa (deixar um espaço com pelo menos 10 linhas) 
-          <textarea className="block" style={{ border: "1px solid black" }} />
-
-          <button type="submit">Marcar</button>
-          <button>Cancelar</button>
-        </form> */}
       </div>
     </div>
   );
