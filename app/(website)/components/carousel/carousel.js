54499ba4dabe5217122f211eefb011e43b8feabc
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-29 10:31:46 +0200

    Carousel

--- app/(website)/components/carousel/carousel.js
+++ app/(website)/components/carousel/carousel.js
@@ -6,28 +6,17 @@ import "swiper/css/pagination";
 
 import "./carousel.css";
 
-import Image from "next/image";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
-import image1 from "@/public/image-1.jpeg";
-import image2 from "@/public/image-2.jpg";
-import image3 from "@/public/image-3.jpeg";
-import image4 from "@/public/image-4.jpg";
-import image5 from "@/public/image-5.jpg";
-import image6 from "@/public/image-6.jpeg";
-import image7 from "@/public/image-7.jpg";
-import image8 from "@/public/image-8.jpeg";
-import image9 from "@/public/image-9.jpg";
 import Link from "next/link";
 import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
 
-export default function Carousel() {
+export default function Carousel({ news }) {
   const pagination = {
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
+      return '<span className="' + className + '">' + (index + 1) + '</span>';
     },
   };
 
@@ -40,164 +29,23 @@ export default function Carousel() {
       modules={[Navigation, Pagination, Mousewheel, Keyboard]}
       pagination={pagination}
     >
-      <SwiperSlide>
-        <Image
-          src={image3}
-          alt="Image3"
-          quality={100}
-        />
-        <div>
-          <h2>
-            Iniciou a 18ª Coorte do curso de Mestrado em Saúde Publica na Faculdade de Medicina na sua edição de 2023-2024.
-          </h2>
-          <p>
-            No dia 20 de Fevereiro de 2023, realizou.se a cerimónia de abertura do curso de Mestrado em Saúde Pública na 
-            Faculdade de Medicina da Universidade Eduardo Mondlane. Fazem parte desta coorte, 34 estudantes de várias áreas 
-            de saúde e afins.
-          </p>
-          <Link href="/">
-            Ver mais <DoubleArrowRightIcon />
-          </Link>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <Image
-          src={image1}
-          alt="Image1"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image2}
-          alt="Image2"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      
-      <SwiperSlide>
-        <Image
-          src={image4}
-          alt="Image4"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image5}
-          alt="Image5"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image6}
-          alt="Image6"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image7}
-          alt="Image7"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image8}
-          alt="Image8"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image9}
-          alt="Image9"
-          quality={100}
-        />
-        <div>
-          <h2>Professor Catedrático Albertino Damasceno e sua equipa recebem Menção Honrosa -Prémio Bial</h2>
-          <p>
-            Professor Catedrático Albertino Damasceno da Faculdade de Medicina da Universidade Eduardo 
-            Mondlane e médico cardiologista do Hospital Central de Maputo que ao longo de 25 anos trabalha 
-            na investigação alargada sobre hipertensão arterial em Moçambique recebeu no dia 8 de Fevereiro 
-            de 2023, a Mençã
-          </p>
-          {/* Link */}
-        </div>
-      </SwiperSlide>
+      {news.map(function(news) {
+        return (
+          <SwiperSlide key={news._id}>
+            <img
+              src={`http://localhost:3001/${news.image}`}
+              alt="Image3"
+            />
+            <div>
+              <h2 className="text-[red]">{news.title}</h2>
+              <p>{news.description}</p>
+              <Link href={`/noticias/${news._id}`}>
+                Ver mais <DoubleArrowRightIcon />
+              </Link>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 }
--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import Carousel from "./components/carousel/carousel";
+import { getNews } from "../api/server";
 
-export default function Page() {
+export default async function Page() {
+  const news = await getNews();
+
   return (
-    <main>
-      <Carousel />
-      <Link href="/sobre-a-faculdade">Sobre</Link>
+    <main className="pl-28 pr-28">
+      <div className="pl-3 pr-3">
+        <Carousel news={news} />
+        <Link href="/sobre-a-faculdade">Sobre</Link>
+      </div>
     </main>
   );
 }
